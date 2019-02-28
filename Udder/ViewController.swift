1863219ed7ce0cee2c6a999b6fec599facb713a4
James Johnson <jerryle3000@gmail.com>
2019-02-28 15:21:51 -0600

    Add ViewDid Appear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -18,5 +18,9 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         <#code#>
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        <#code#>
+    }
 }
 
