c1c35824e01cc9f81b65f0379ce696ecd3420036
James Johnson <jerryle3000@gmail.com>
2019-02-28 14:53:09 -0600

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by John Smith on 2/28/19.
+//  Copyright © 2019 com.jerryle.magic8ball. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
